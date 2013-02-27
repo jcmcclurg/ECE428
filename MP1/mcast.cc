bb2cbf5e00313753510601128280f63b027522ae
Yihua Lou <yihualou92@gmail.com>
2013-02-27 14:56:05 -0600

    Fixed timestamp serialization
    Scrapped old serialization method. Newer interface is much cleaner and supports piggybacking multiple strings.
    Still need to test for memory leaks on the EWS machines.

--- MP1/mcast.cc
+++ MP1/mcast.cc
@@ -2,31 +2,77 @@
 #include <assert.h>
 #include <stdio.h>
 #include <map>
+#include <vector>
 
 #include "mp1.h"
 
 /* Defines */
 
-#define PIGGYBACK_DELIMITER '\x17'
+#define MESSAGE_HEADER '\x1f'
+#define TIMESTAMP_HEADER '\x1d'
 
 #define HEARTBEAT_DELAY 10
 #define HEARTBEAT_MESSAGE "xoxo"
 
 /* General */
 
-// Returns a newly allocated string. Remember to delete it.
-char* piggy_back(const char* message, const char* aux) {
-    // Extra two characters for delimiter and null
-    int message_len = strlen(message);
-    int aux_len = strlen(aux);
+// Flattens a bunch of strings and a timestamp into a serialized string.
+void flatten(
+        std::vector<const char*>& messages,
+        int* timestamp,
+        char*& combined_message,
+        int& total_bytes) {
 
-    char* new_message = (char*) calloc(strlen(message) + strlen(aux) + 2, 1);
-    strcpy(new_message, message);
-    new_message[message_len] = PIGGYBACK_DELIMITER;
-    strcpy(new_message + message_len + 1, aux);
-    new_message[message_len + 1 + aux_len] = 0;
+    int *message_bytes = (int*) calloc(messages.size(), sizeof(int)); 
+    int timestamp_bytes = mcast_num_members * sizeof(int);
 
-    return new_message;
+    total_bytes = 0;
+    for (int i = 0; i < messages.size(); ++i) {
+        int bytes = strlen(messages[i]) + 1;
+        message_bytes[i] = bytes;
+        total_bytes += bytes;
+    }
+    // The extra bytes are for the control headers. 
+    total_bytes += messages.size() + 1 + timestamp_bytes;
+
+    combined_message = (char*) calloc(total_bytes, 1);
+
+    int offset = 0;
+    for (int i = 0; i < messages.size(); ++i) {
+        combined_message[offset] = MESSAGE_HEADER;
+        offset++;
+        memcpy(combined_message + offset, messages[i], message_bytes[i]);
+        offset += message_bytes[i];
+    }
+
+    // Add timestamp
+    combined_message[offset] = TIMESTAMP_HEADER;
+    offset++;
+    memcpy(combined_message + offset, timestamp, timestamp_bytes);
+
+    free(message_bytes);
+}
+
+void unflatten(
+        const char* combined_message,
+        int total_bytes,
+        std::vector<const char*>& messages,
+        int*& timestamp) {
+
+    int offset = 1;
+    while (offset < total_bytes) {
+        if (combined_message[offset - 1] == MESSAGE_HEADER) {
+            int bytes = strlen(combined_message + offset) + 1;
+            messages.push_back(combined_message + offset);
+            offset += bytes + 1;
+        } else if (combined_message[offset - 1] == TIMESTAMP_HEADER) {
+            timestamp = (int*) (combined_message + offset);
+            return;
+        } else {
+            printf("Error! Poorly formatted message!\n");
+            return;
+        }
+    }
 }
 
 std::map<int, int> id_map;
@@ -42,18 +88,12 @@ void id_map_init(void) {
 int *timestamp;
 
 void timestamp_init(void) {
-    // Extra slot for easy stringification.
-    timestamp = (int*) calloc(mcast_num_members + 1, sizeof(int));
-    memset(timestamp, 0, mcast_num_members + 1);
+    timestamp = (int*) calloc(mcast_num_members, sizeof(int));
+    memset(timestamp, 1, mcast_num_members);
 }
 
 void timestamp_increment() {
     timestamp[id_map[my_id]]++;
-}
-
-// Ultra hacky int array stringification.
-const char* timestamp_stringify() {
-    return (const char*) timestamp;
 }
 
 void timestamp_destroy() {
@@ -94,21 +134,35 @@ void multicast_init(void) {
 /* Basic multicast implementation */
 void multicast(const char *message) {
     timestamp_increment();
-    char *new_message = piggy_back(message, timestamp_stringify());
+    
+    char* combined_message = NULL;
+    int total_bytes;
 
-    int i;
+    std::vector<const char*> messages;
+    messages.push_back("abc");
+    messages.push_back("omg");
+
+    flatten(messages, timestamp, combined_message, total_bytes);
 
     pthread_mutex_lock(&member_lock);
-    for (i = 0; i < mcast_num_members; i++) {
-        usend(mcast_members[i], message, strlen(message)+1);
+    for (int i = 0; i < mcast_num_members; i++) {
+        usend(mcast_members[i], combined_message, total_bytes);
     }
     pthread_mutex_unlock(&member_lock);
 
-    free(new_message);
+    free(combined_message);
 }
 
 void receive(int source, const char *message, int len) {
-    assert(message[len-1] == 0);
+    //assert(message[len-1] == 0);
+
+    std::vector<const char*> messages;
+    int* other_timestamp;    
+    unflatten(message, len, messages, other_timestamp);
+
+    printf("other timestamp: %d\n", other_timestamp[id_map[my_id]]);
+    printf("last timestamp: %d\n", other_timestamp[mcast_num_members - 1]);
+
     deliver(source, message);
 }
 

33417240cac9b5cb8d79b730b6f8a1691025d7ee
Josiah McClurg <jcmcclu2@linux7.ews.illinois.edu>
2013-05-01 12:17:29 -0500

    adding clients

---
+++ MP2/src/client/client2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+#include "frontend.h"
+#include "replica/replicas.h"
+#include "settings.h"
+#include <iostream>
+#include <string>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/thread/thread.hpp>
+
+using namespace mp2;
+using namespace std;
+using boost::shared_ptr;
+
+int main(int argc, char **argv) {
+	shared_ptr<Replicas> replicas(new Replicas(argc, argv));
+
+	FrontEnd frontEnd0(replicas, 0);
+//	FrontEnd frontEnd1(replicas, 1);
+
+	try {
+		shared_ptr<StateMachine> machine0 = frontEnd0.get("s0");
+
+		DEBUG("\nReads and writes succeed after 1 or 2 failures.");
+		DEBUG("read s0:" << machine0->getState());
+		DEBUG("Please exit replicas 1 and 2.");
+	} catch (ReplicaError e) {
+			cerr << e.message << endl;
+			return 1;
+	}
+
+	return 0;
+}
